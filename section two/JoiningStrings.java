9873a321fcc4bca99094009e6017011489b3cc2c
Andrew Liam C <andrewliamc@startmail.com>
2022-12-29 10:11:54 -0500

    Create file for joining strings in Java.
    Create Java file, add main method, and join multiple strings.

---
+++ section two/JoiningStrings.java
@@ -0,0 +1,12 @@
+public class JoiningStrings {
+
+  public static void main(String[] args) {
+
+    int year = 2012;
+    String winner = "Spain";
+
+    String announcement = "The winner of the " + year + " World Cup are " + winner + ".";
+    System.out.println(announcement);
+  }
+
+}
