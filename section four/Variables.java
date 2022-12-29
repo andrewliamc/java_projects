e999af910ac62d81fe854a648cfe2ad991f36a04
Andrew Liam C <andrewliamc@startmail.com>
2022-12-29 10:10:18 -0500

    Change variables
    Use operators to change int passengers variable.

---
+++ section four/Variables.java
@@ -0,0 +1,21 @@
+public class Variables {
+
+  public static void main(String[] args) {
+    int passengers = 5;
+    System.out.println(passengers);
+
+    passengers += 9;
+    System.out.println(passengers);
+
+    passengers -= 5;
+    System.out.println(passengers);
+
+    passengers -= 4;
+    System.out.println(passengers);
+
+    int busTickets = passengers;
+    System.out.println(busTickets);
+
+  }
+
+}
