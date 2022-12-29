d8993510579004b4cfa93a710af796f72ae48e46
Andrew Liam C <andrewliamc@startmail.com>
2022-12-29 08:27:53 -0500

    Remove package.
    Remove package from HelloJava.java file.

--- section one/HelloJava.java
+++ section one/HelloJava.java
@@ -1,8 +1,7 @@
-package section_one;
-
 public class HelloJava {
   
   public static void main(String[] args) {
+    System.out.println("Hello Java!");
     System.out.println("Hello Java!");
   }
 }
